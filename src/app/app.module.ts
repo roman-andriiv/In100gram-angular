2bf37228c45fafe04cc6912ae21d570ad4609e68
Roman Andriiv <roman.andriyiv@gmail.com>
2023-05-05 10:54:21 +0200

    [andriiv][develop] implement AuthInterceptorProvider and add it to app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {MaterialModule} from "./material-module";
 import {HttpClientModule} from "@angular/common/http";
 import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {authInterceptorProvider} from "./helper/auth-interceptor.service";
 
 @NgModule({
   declarations: [
@@ -21,7 +22,7 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
     FormsModule,
     ReactiveFormsModule
   ],
-  providers: [],
+  providers: [authInterceptorProvider],
   bootstrap: [AppComponent]
 })
 export class AppModule {

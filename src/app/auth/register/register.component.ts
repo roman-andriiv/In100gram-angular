550cc8bfd6ff4eb3080ec031b3c4170c35c79bfa
Roman Andriiv <roman.andriyiv@gmail.com>
2023-05-07 14:57:22 +0200

    [andriiv][develop] implement RegisterComponents

--- src/app/auth/register/register.component.ts
+++ src/app/auth/register/register.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AuthService} from "../../services/auth.service";
+import {NotificationService} from "../../services/notification.service";
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
   styleUrls: ['./register.component.css']
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
 
+  public registerForm: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private notificationService: NotificationService,
+    private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+    this.registerForm = this.createRegisterForm();
+  }
+
+  createRegisterForm(): FormGroup {
+    return this.fb.group({
+      email: ['', Validators.compose([Validators.required, Validators.email])],
+      username: ['', Validators.compose([Validators.required])],
+      firstName: ['', Validators.compose([Validators.required])],
+      lastName: ['', Validators.compose([Validators.required])],
+      password: ['', Validators.compose([Validators.required])],
+      confirmPassword: ['', Validators.compose([Validators.required])],
+    });
+  }
+
+  submit() {
+    console.log(this.registerForm.value);
+
+    this.authService.register({
+      email: this.registerForm.value.email,
+      username: this.registerForm.value.username,
+      firstName: this.registerForm.value.firstName,
+      lastName: this.registerForm.value.lastName,
+      password: this.registerForm.value.password,
+      confirmPassword: this.registerForm.value.confirmPassword,
+    }).subscribe(data => {
+      console.log(data);
+      this.notificationService.showSnackBar('Successfully registered');
+    }, error => {
+      console.log(error);
+      this.notificationService.showSnackBar('Something went wrong during registration');
+    });
+  }
 }
